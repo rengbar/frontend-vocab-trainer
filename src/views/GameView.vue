20bc3acfa72dd55382a7db7921d4f4b25b50b409
rengbar <rengbar.hardam@gmail.com>
2023-07-29 14:38:07 +0200

    implemented finishing the game in GameView

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="game-view">
+  <div v-if="!gameEnded" class="game-view">
     <h1 class="text-center">{{ title }}</h1>
     <div class="question">
       {{ question }}
@@ -8,6 +8,9 @@
       <button class="btn btn-primary" v-for="(answer, index) in answers" :key="index" :class="{ 'btn-success': answer.correct && answer.clicked, 'btn-danger': !answer.correct && answer.clicked }" @click="checkAnswer(index)">{{ answer.name }}</button>
     </div>
     <div class="feedback" v-if="feedback">{{ feedback }}</div>
+  </div>
+  <div v-else>
+    <h1>Test</h1>
   </div>
 </template>
 
@@ -22,6 +25,7 @@ const question = ref("");
 const answers = ref([]);
 const feedback = ref("");
 const currentRound = ref(0);
+const gameEnded = ref(false);
 
 const shuffleArray = (array) => {
   for (let i = array.length - 1; i > 0; i--) {
@@ -36,6 +40,10 @@ const checkAnswer = async (index) => {
     feedback.value = "The answer was correct!";
   } else {
     feedback.value = "The answer was incorrect!";
+    const correctAnswerIndex = answers.value.findIndex(answer => answer.correct);
+    if (correctAnswerIndex !== -1) {
+      answers.value[correctAnswerIndex].clicked = true;
+    }
   }
 
   var requestOptions = {
@@ -50,39 +58,56 @@ const checkAnswer = async (index) => {
 
   setTimeout(() => {
     feedback.value = "";
+    answers.value.forEach(answer => answer.clicked = false);
     currentRound.value++;
-    loadRound();
-  }, 5000);
+    if (currentRound.value === gameData.value.length) {
+      gameEnded.value = true;
+      endGame();
+    } else {
+      loadRound();
+    }
+  }, 1500);
 };
+
+const endGame = async () => {
+  const requestOptions = {
+    method: 'PATCH',
+    redirect: 'follow'
+  };
+
+  while (true) {
+    await new Promise(resolve => setTimeout(resolve, 1500));
+    const response = await fetch(`http://localhost:8080/rest/game/end/${store.gameId}?user_id=${store.userId}`, requestOptions);
+    if (response.ok) {
+      const result = await response.json();
+      console.log(result);
+      break;
+    } else {
+      console.log('An error occurred, retrying...');
+    }
+  }
+};
+
+
 
 const loadRound = () => {
   if (currentRound.value >= gameData.value.length) {
-    // Alle Runden sind abgeschlossen, Sie können hier eine Meldung anzeigen oder etwas anderes tun
     return;
   }
 
   const roundData = gameData.value[currentRound.value];
-
-  // Ändern Sie den Titel
   title.value = "Choose the correct answer by clicking the button!";
-
-  // Setzen Sie die Frage
   const name = roundData.question.vocabulary.name;
-  question.value = `Which one is the right translation for ${name}?`;
-
-  // Leeren Sie die vorherigen Antworten
+  question.value = `Which one is the right translation for "${name}"?`;
   answers.value = [];
 
-  // Fügen Sie die Antwortmöglichkeiten hinzu
   roundData.question.answers.forEach(answer => {
     answers.value.push({ name: answer.name, correct: false, clicked: false, translationId: answer.translationId });
   });
 
-  // Fügen Sie die korrekte Antwort hinzu
   const correctAnswer = roundData.question.vocabulary.translations[0].synonyms[0];
   answers.value.push({ name: correctAnswer.name, correct: true, clicked: false, translationId: correctAnswer.translationId });
 
-  // Mischen Sie die Antworten
   shuffleArray(answers.value);
 };
 
@@ -92,6 +117,7 @@ const fetchGameData = async () => {
   const response = await fetch(`http://localhost:8080/rest/game/start/${gameId}`);
   if (response.ok) {
     gameData.value = await response.json();
+    console.log('Game data loaded successfully', gameData.value.length);
     loadRound();
   } else {
     console.log('An error occurred');
@@ -106,17 +132,18 @@ onMounted(fetchGameData);
 
 <style scoped>
 .text-center {
-  margin-top: 20px; /* Abstand zur Navbar */
+  margin-top: 20px;
 }
 
 .question {
-  text-align: center; /* Zentriert den Text */
-  font-size: 1.5em; /* Erhöht die Schriftgröße */
-  margin-top: 20px; /* Abstand zur Überschrift */
+  text-align: center;
+  font-size: 1.5em;
+  margin-top: 20px;
+  margin-bottom: 20px;
 }
 
 .feedback {
-  text-align: center; /* Zentriert den Text */
-  margin-top: 20px; /* Abstand zur Antwortliste */
+  text-align: center;
+  margin-top: 20px;
 }
 </style>
